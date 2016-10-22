379a44da89cdedde05745b58214ccb71263954df
I-M-I <llukechadwick@gmail.com>
2016-10-22 14:11:01 +1300

    First commit

---
+++ AOBSigMaker/AOBSigMaker.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace AOBSigMaker
+{
+    partial class AOBSigMaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResultMask = new System.Windows.Forms.TextBox();
+            this.arrayB = new System.Windows.Forms.TextBox();
+            this.arrayA = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.btnCopyArrayA = new System.Windows.Forms.Button();
+            this.btnCopyArrayB = new System.Windows.Forms.Button();
+            this.btnCopyHalfMask = new System.Windows.Forms.Button();
+            this.btnCopyResult = new System.Windows.Forms.Button();
+            this.btnCopyStyle1 = new System.Windows.Forms.Button();
+            this.btnCopyStyle2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCopyFullMask = new System.Windows.Forms.Button();
+            this.txtMaskWhole = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtResultMask
+            // 
+            this.txtResultMask.Location = new System.Drawing.Point(86, 124);
+            this.txtResultMask.Name = "txtResultMask";
+            this.txtResultMask.Size = new System.Drawing.Size(316, 20);
+            this.txtResultMask.TabIndex = 9;
+            // 
+            // arrayB
+            // 
+            this.arrayB.Location = new System.Drawing.Point(86, 31);
+            this.arrayB.Name = "arrayB";
+            this.arrayB.Size = new System.Drawing.Size(316, 20);
+            this.arrayB.TabIndex = 8;
+            // 
+            // arrayA
+            // 
+            this.arrayA.Location = new System.Drawing.Point(86, 5);
+            this.arrayA.Name = "arrayA";
+            this.arrayA.Size = new System.Drawing.Size(316, 20);
+            this.arrayA.TabIndex = 7;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(86, 98);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(316, 20);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(86, 63);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 22);
+            this.btnCompare.TabIndex = 5;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(86, 175);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(316, 20);
+            this.txtCode.TabIndex = 10;
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(86, 201);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(316, 20);
+            this.txtArray.TabIndex = 11;
+            // 
+            // btnCopyArrayA
+            // 
+            this.btnCopyArrayA.Location = new System.Drawing.Point(408, 4);
+            this.btnCopyArrayA.Name = "btnCopyArrayA";
+            this.btnCopyArrayA.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyArrayA.TabIndex = 12;
+            this.btnCopyArrayA.Text = "Copy";
+            this.btnCopyArrayA.UseVisualStyleBackColor = true;
+            this.btnCopyArrayA.Click += new System.EventHandler(this.btnCopyArrayA_Click);
+            // 
+            // btnCopyArrayB
+            // 
+            this.btnCopyArrayB.Location = new System.Drawing.Point(408, 30);
+            this.btnCopyArrayB.Name = "btnCopyArrayB";
+            this.btnCopyArrayB.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyArrayB.TabIndex = 13;
+            this.btnCopyArrayB.Text = "Copy";
+            this.btnCopyArrayB.UseVisualStyleBackColor = true;
+            this.btnCopyArrayB.Click += new System.EventHandler(this.btnCopyArrayB_Click);
+            // 
+            // btnCopyHalfMask
+            // 
+            this.btnCopyHalfMask.Location = new System.Drawing.Point(408, 123);
+            this.btnCopyHalfMask.Name = "btnCopyHalfMask";
+            this.btnCopyHalfMask.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyHalfMask.TabIndex = 15;
+            this.btnCopyHalfMask.Text = "Copy";
+            this.btnCopyHalfMask.UseVisualStyleBackColor = true;
+            this.btnCopyHalfMask.Click += new System.EventHandler(this.btnCopyHalfMask_Click);
+            // 
+            // btnCopyResult
+            // 
+            this.btnCopyResult.Location = new System.Drawing.Point(408, 97);
+            this.btnCopyResult.Name = "btnCopyResult";
+            this.btnCopyResult.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyResult.TabIndex = 14;
+            this.btnCopyResult.Text = "Copy";
+            this.btnCopyResult.UseVisualStyleBackColor = true;
+            this.btnCopyResult.Click += new System.EventHandler(this.btnCopyResult_Click);
+            // 
+            // btnCopyStyle1
+            // 
+            this.btnCopyStyle1.Location = new System.Drawing.Point(408, 174);
+            this.btnCopyStyle1.Name = "btnCopyStyle1";
+            this.btnCopyStyle1.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyStyle1.TabIndex = 16;
+            this.btnCopyStyle1.Text = "Copy";
+            this.btnCopyStyle1.UseVisualStyleBackColor = true;
+            this.btnCopyStyle1.Click += new System.EventHandler(this.btnCopyStyle1_Click);
+            // 
+            // btnCopyStyle2
+            // 
+            this.btnCopyStyle2.Location = new System.Drawing.Point(408, 200);
+            this.btnCopyStyle2.Name = "btnCopyStyle2";
+            this.btnCopyStyle2.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyStyle2.TabIndex = 17;
+            this.btnCopyStyle2.Text = "Copy";
+            this.btnCopyStyle2.UseVisualStyleBackColor = true;
+            this.btnCopyStyle2.Click += new System.EventHandler(this.btnCopyStyle2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Byte Array A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Byte Array B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Mask Half-Byte";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Style 1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Style 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Mask Full-Byte";
+            // 
+            // btnCopyFullMask
+            // 
+            this.btnCopyFullMask.Location = new System.Drawing.Point(408, 148);
+            this.btnCopyFullMask.Name = "btnCopyFullMask";
+            this.btnCopyFullMask.Size = new System.Drawing.Size(47, 22);
+            this.btnCopyFullMask.TabIndex = 27;
+            this.btnCopyFullMask.Text = "Copy";
+            this.btnCopyFullMask.UseVisualStyleBackColor = true;
+            this.btnCopyFullMask.Click += new System.EventHandler(this.btnCopyFullMask_Click);
+            // 
+            // txtMaskWhole
+            // 
+            this.txtMaskWhole.Location = new System.Drawing.Point(86, 149);
+            this.txtMaskWhole.Name = "txtMaskWhole";
+            this.txtMaskWhole.Size = new System.Drawing.Size(316, 20);
+            this.txtMaskWhole.TabIndex = 25;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(167, 64);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(114, 20);
+            this.txtStatus.TabIndex = 30;
+            // 
+            // AOBSigMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 225);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCopyFullMask);
+            this.Controls.Add(this.txtMaskWhole);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopyStyle2);
+            this.Controls.Add(this.btnCopyStyle1);
+            this.Controls.Add(this.btnCopyHalfMask);
+            this.Controls.Add(this.btnCopyResult);
+            this.Controls.Add(this.btnCopyArrayB);
+            this.Controls.Add(this.btnCopyArrayA);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtResultMask);
+            this.Controls.Add(this.arrayB);
+            this.Controls.Add(this.arrayA);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCompare);
+            this.Name = "AOBSigMaker";
+            this.Text = "AOB SigMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResultMask;
+        private System.Windows.Forms.TextBox arrayB;
+        private System.Windows.Forms.TextBox arrayA;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.Button btnCopyArrayA;
+        private System.Windows.Forms.Button btnCopyArrayB;
+        private System.Windows.Forms.Button btnCopyHalfMask;
+        private System.Windows.Forms.Button btnCopyResult;
+        private System.Windows.Forms.Button btnCopyStyle1;
+        private System.Windows.Forms.Button btnCopyStyle2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCopyFullMask;
+        private System.Windows.Forms.TextBox txtMaskWhole;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
---
+++ AOBSigMaker/AOBSigMaker.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Windows.Forms;
+//AOB SigMaker - https://github.com/I-M-I/AOBSigMaker
+
+namespace AOBSigMaker
+{
+    public partial class AOBSigMaker : Form
+    {
+        public AOBSigMaker()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            string ArrayA = arrayA.Text;
+            string ArrayB = arrayB.Text;
+            int ArrayLength = ArrayA.Length;          
+            
+            //Clear text, mainly for preventing duplicates when clicked again
+            txtResult.Clear();
+            txtResultMask.Clear();
+            txtMaskWhole.Clear();
+            txtArray.Clear();
+            txtCode.Clear();
+
+
+            if (arrayA.Text.Length != arrayB.Text.Length)
+            {
+                txtStatus.Text = "Array Size Mismatch!";
+                return;
+            }
+
+
+            //Make array with wildcards (Half-Byte)
+            for (int i = 0; i < ArrayLength; i++)
+            {
+
+                if (ArrayA[i] != ArrayB[i])
+                {
+                    txtResult.Text = txtResult.Text + "?";
+                }
+                else
+                {
+                    txtResult.Text = txtResult.Text + ArrayA[i];
+                }
+
+            }
+
+            //Make Mask
+            
+            string arrayAnospaces = arrayA.Text.Replace(" ", "");
+            string arrayBnospaces = arrayB.Text.Replace(" ", "");
+            int ArrayLengthMask = arrayAnospaces.Length;
+
+            for (int i = 0; i < ArrayLengthMask; i++)
+            {
+
+
+                if (arrayAnospaces[i] != arrayBnospaces[i])
+                {
+                    txtResultMask.Text = txtResultMask.Text + "?";
+                }
+                else
+                {
+                    txtResultMask.Text = txtResultMask.Text + "x";
+                }
+
+            }
+
+
+
+       //Make array with wildcards (Whole-Byte)
+            string FirstHalf ="";
+            string SecondHalf = "";
+
+
+            //Compare the first character of each byte
+            for (int i = 0; i < ArrayLengthMask; i += 2)
+            {
+
+
+                if (arrayAnospaces[i] != arrayBnospaces[i])
+                {
+                    FirstHalf = FirstHalf + "?";
+                }
+                else
+                {
+                    FirstHalf = FirstHalf + "x";
+                }
+
+            }
+
+            //Compare the second character of each byte
+            for (int i = 1; i < ArrayLengthMask; i += 2)
+            {
+
+
+                if (arrayAnospaces[i] != arrayBnospaces[i])
+                {
+                    SecondHalf = SecondHalf + "?";
+                }
+                else
+                {
+                    SecondHalf = SecondHalf + "x";
+                }
+
+            }
+
+            string ArrayAS = FirstHalf;
+            string ArrayBS = SecondHalf;
+
+            //Merge the two halves 
+            for (int i = 0; i < ArrayLengthMask / 2; i++)
+            {
+
+                if (ArrayAS[i] != ArrayBS[i])
+                {
+                    txtMaskWhole.Text = txtMaskWhole.Text + "?";
+                }
+                else
+                {
+                    txtMaskWhole.Text = txtMaskWhole.Text + ArrayAS[i];
+                }
+
+            }
+
+
+            //Convert to Style 1
+            string Style1 = arrayA.Text.Replace(" ", ", 0x");
+
+            txtArray.Text = "0x" + Style1;
+
+            //Convert to Style 2
+            string Style2 = arrayA.Text.Replace(" ", @"\x"); ;
+
+            txtCode.Text = @"\x" + Style2;
+
+            txtStatus.Text = "OK!";
+        }
+
+        private void btnCopyArrayA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(arrayA.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyArrayB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(arrayB.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtResult.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyHalfMask_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtResultMask.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyFullMask_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtMaskWhole.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyStyle1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtCode.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+
+        private void btnCopyStyle2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtArray.Text);
+                txtStatus.Text = "Copied to Clipboard";
+            }
+            catch
+            {
+                txtStatus.Text = "Nothing to Copy";
+            }
+        }
+    }
+}
